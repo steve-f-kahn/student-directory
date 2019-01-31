f0340cbe5feb65667253bc78689b3738385f3722
Steve_Kahn <steve@kahn.org.uk>
2019-01-31 15:52:55 +0000

    Sorted list of students by cohort and made the statement on input gramatically correct

--- directory.rb
+++ directory.rb
@@ -6,9 +6,13 @@ def input_students
   cohort = gets.chomp.to_sym
   while !name.empty? do
     students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    if !students.count == 1
+      puts "Now we have #{students.count} students"
+    else
+      puts "Now we have #{students.count} student"
+    end
     name = gets.chomp
-    cohort = gets.chomp if !name.empty?
+    cohort = gets.chomp.to_sym if !name.empty?
   end
 	students
 end
@@ -20,12 +24,56 @@ def print_header
 end
 
 def print(students)
-  track = 0
-  while track < students.count 
-    if students[track][:name].length < 12
-      puts "#{track+1}. #{students[track][:name]} (#{students[track][:cohort]} cohort)".center($header.length)
+  cohort = {
+    :January => [],
+    :Febuary => [],
+    :March => [],
+    :April => [],
+    :May => [],
+    :June => [],
+    :July => [],
+    :August => [],
+    :September => [],
+    :October => [],
+    :November => [],
+    :December => []
+  }
+  students.each do |student|
+    case student[:cohort]
+      when :January
+        cohort[:January] << student[:name]
+      when :Febuary
+        cohort[:Febuary] << student[:name]
+      when :March
+        cohort[:March] << student[:name]
+      when :April
+        cohort[:April] << student[:name]
+      when :May
+        cohort[:May] << student[:name]
+      when :June
+        cohort[:June] << student[:name]
+      when :July
+        cohort[:July] << student[:name]
+      when :August
+        cohort[:August] << student[:name]
+      when :September
+        cohort[:September] << student[:name]
+      when :October
+        cohort[:October] << student[:name]
+      when :November
+        cohort[:November] << student[:name]
+      when :December
+        cohort[:December] << student[:name]
+      end
+  end
+  index = 1
+  cohort.each do |month, student|
+    student.each do |name|
+      if name.length < 12
+        puts "#{index}. #{name} (#{month} cohort)".center($header.length)
+        index += 1
+      end
     end
-    track += 1
   end
 end
 
