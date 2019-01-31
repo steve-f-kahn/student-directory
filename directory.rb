8f9e693c5e9b12b8d4f646694e64f7b15cd681a6
Steve_Kahn <steve@kahn.org.uk>
2019-01-31 12:48:44 +0000

    Changed student list to array, refactered putting of students, split into methods for clarity

--- directory.rb
+++ directory.rb
@@ -1,16 +1,27 @@
-puts "The students of Villains Academy"
-puts "------------"
-puts "Dr. Hannbal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have"
-print 11
-puts " great students"
+students = [
+  "Dr. Hannbal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+def print_header
+  puts "The students of Villains Academy"
+  puts  "-------------"
+end
+def print(names)
+  names.each { |name| puts name }
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students "
+end
+
+print_header
+print(students)
+print_footer(students)
