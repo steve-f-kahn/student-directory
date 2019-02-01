351486447cf9014e7b047f84b1cf5998a2cc3cd9
Steve_Kahn <steve@kahn.org.uk>
2019-02-01 13:05:18 +0000

    Added option to save students to file

--- directory.rb
+++ directory.rb
@@ -37,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save students"
   puts "9. Exit"
 end
 
@@ -52,10 +53,23 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else 
       puts "I don't know what you meant, try again"
   end
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 interactive_menu
